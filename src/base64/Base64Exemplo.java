af67604341b255234d130329eeca3623b9a08643
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-16 10:41:34 -0300

    Base64

---
+++ src/base64/Base64Exemplo.java
@@ -0,0 +1,27 @@
+package base64;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class Base64Exemplo {
+
+	public static void main(String[] args) {
+
+		try {
+
+			String texto = "A classe Base64 no Java 8!";
+			System.out.println(texto);
+
+			String textoCodificado = Base64.getEncoder().encodeToString(texto.getBytes("utf-8"));
+			System.out.println(textoCodificado);
+
+			String textoDecodificado = new String(Base64.getDecoder().decode(textoCodificado), "utf-8");
+			System.out.println(textoDecodificado);
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
